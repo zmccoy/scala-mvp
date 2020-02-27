444e196abf8edf3fff19325085223e86c40f198f
Zach McCoy <zach.mccoy@banno.com>
2020-02-26 18:07:58 -0600

    Add log capabilities and a tiny log at startup for now

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -3,6 +3,7 @@ package com.zmccoy
 import cats.effect._
 import cats.implicits._
 import com.zmcccoy.DatabaseOps
+import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
 import org.http4s.HttpRoutes
 import org.http4s.syntax._
 import org.http4s.dsl.Http4sDsl
@@ -16,6 +17,8 @@ object Server {
   def serve[F[_]: ConcurrentEffect : ContextShift : Timer]: Stream[F, ExitCode] = {
     for {
       transactor <- Stream.resource(DatabaseOps.createTransactor())
+      logger     <- Stream.eval(Slf4jLogger.create[F]) //Could also summon from the instance.
+      _          <- Stream.eval(logger.info("Starting the application"))
       exitCode   <- createPingRoute[F](routes)
     } yield {
       exitCode
@@ -24,7 +27,7 @@ object Server {
   }
 
   /*
-  TODO:  Add trait that uses DB, add new routes, add health server , add config, add logger, add prometheus, add natchez, add dump of threads on end
+  TODO:  Add trait that uses DB, add new routes, add health server , add config, add prometheus, add natchez, add dump of threads on end
   */
 
   //TODO: Move out these route
