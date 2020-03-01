1149059a89a8a4f0953155c0927238b4b0582c78
Zach McCoy <zach.mccoy@banno.com>
2020-02-29 20:48:06 -0600

    Finish adding config and use configs in hikari

--- src/main/scala/DatabaseOps.scala
+++ src/main/scala/DatabaseOps.scala
@@ -1,20 +1,23 @@
 package com.zmcccoy
 
 import cats.effect._
+import com.zmccoy.Server.AppConfig
+import doobie._
+import doobie.implicits._
 import doobie.hikari._
 import doobie.util.ExecutionContexts
 
 object DatabaseOps {
   //TODO: Pass in configuration, write up explanation comments
-  def createTransactor[F[_] : Async : ContextShift](): Resource[F, HikariTransactor[F]] = {
+  def createTransactor[F[_] : Async : ContextShift](config: AppConfig): Resource[F, HikariTransactor[F]] = {
     for {
-      connectEC  <- ExecutionContexts.fixedThreadPool[F](10)
+      connectEC  <- ExecutionContexts.fixedThreadPool[F](config.doobie.connectECSize)
       transactEC <- ExecutionContexts.cachedThreadPool[F]
       transactor <- HikariTransactor.newHikariTransactor[F](
-        "org.postgresql.Driver",
-        "url",
-        "username",
-        "pass",
+        config.hikari.driverClassName,
+        config.hikari.url,
+        config.hikari.user,
+        config.hikari.pass,
         connectEC,
         Blocker.liftExecutionContext(transactEC))
     } yield transactor
--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -2,6 +2,7 @@ package com.zmccoy
 
 import cats.effect._
 import cats.implicits._
+import com.typesafe.config.ConfigFactory
 import com.zmcccoy.DatabaseOps
 import org.http4s.HttpRoutes
 import org.http4s.syntax._
@@ -11,6 +12,8 @@ import org.http4s.server.blaze._
 import fs2.Stream
 import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
 import org.http4s.client.blaze.BlazeClientBuilder
+import pureconfig._
+import pureconfig.generic.auto._
 
 import scala.concurrent.ExecutionContext.global
 
@@ -20,19 +23,19 @@ object Server {
 
   def serve[F[_]: ConcurrentEffect : ContextShift : Timer]: Stream[F, ExitCode] = {
     for {
+      config     <- Stream.eval(loadConfig)
       logger     <- Stream.eval(Slf4jLogger.create)
       _          <- Stream.eval(logger.info("Starting the application"))
-      transactor <- Stream.resource(DatabaseOps.createTransactor())
+      transactor <- Stream.resource(DatabaseOps.createTransactor(config))
       client     <- Stream.resource(BlazeClientBuilder[F](global).resource)
       exitCode   <- createPingRoute[F](routes)
     } yield {
       exitCode
     }
-
   }
 
   /*
-  TODO:  Add trait that uses DB, add new routes, add health server , add config, add logger, add prometheus, add natchez, add dump of threads on end
+  TODO:  Add trait that uses DB, add new routes, add health server, add prometheus, add natchez, add dump of threads on end
   */
 
   //TODO: Move out these route
@@ -52,4 +55,12 @@ object Server {
       .withHttpApp(routes.orNotFound)
       .serve
   }
+
+  final case class DoobieConfig(connectECSize: Int)
+  final case class HConfig(url: String, driverClassName: String, user: String, pass: String)
+  final case class AppConfig(doobie: DoobieConfig, hikari: HConfig)
+  def loadConfig[F[_]: Sync]: F[AppConfig] = Sync[F].delay {
+      val config = ConfigFactory.load()
+      pureconfig.ConfigSource.fromConfig(config).at("com.zmccoy").loadOrThrow[AppConfig]
+    }
 }
